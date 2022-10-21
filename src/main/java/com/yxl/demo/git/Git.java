820b91a08711b6b39f8158b24d831c2b15135dda
yangxiangliu <1910701108@qq.com>
2022-10-21 20:40:21 +0800

    第二次推送

--- src/main/java/com/yxl/demo/git/Git.java
+++ src/main/java/com/yxl/demo/git/Git.java
@@ -10,5 +10,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("1");
         System.out.println("push commit");
+        System.out.println("push commit 2");
     }
 }
